5571dccc566d8ab65842b8dade6e67c94a5820d3
Carlos Calderon <cdcalderon@gmail.com>
2017-03-26 18:26:55 -0500

    initial commit

--- QuoteTicker/MainWindow.xaml.cs
+++ QuoteTicker/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,11 @@ namespace QuoteTicker
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetQuotesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(8000);
         }
     }
 }
